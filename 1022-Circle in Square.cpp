4f2094e0c1f391088247d1832ff75fe3eb033564
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2020-07-12 23:59:42 +0600

    Create 1022-Circle in Square.cpp

---
+++ 1022-Circle in Square.cpp
@@ -0,0 +1,30 @@
+/*BISMILLAH
+THE WHITE WOLF
+NO DREAM IS TOO BIG AND NO DREAMER IS TOO SMALL*/
+
+#include <stdio.h>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    double r;
+    double sarea;
+    double carea;
+    double area;
+    int t;
+ 
+    scanf("%d", &t);
+ 
+    for (int i = 1; i <= t; i++) {
+        scanf("%lf", &r);
+        carea = 2 * acos(0.0) * r * r;
+        sarea = (2 * r) * (2 * r);
+        area = sarea - carea;
+ 
+        printf("Case %d: %.2lf\n",i, area);
+    }
+ 
+ 
+}
