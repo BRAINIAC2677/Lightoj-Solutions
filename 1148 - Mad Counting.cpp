b657f46a1411e5c2e74e213769e3d25c9a30ac8d
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2020-07-13 01:07:18 +0600

    Create 1148 - Mad Counting.cpp

---
+++ 1148 - Mad Counting.cpp
@@ -0,0 +1,26 @@
+/*BISMILLAH
+THE WHITE WOLF
+NO DREAM IS TOO BIG AND NO DREAMER IS TOO SMALL*/
+
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    int tc, cs = 0, i, j, k, n, a[50], res;
+    scanf(" %d", &tc);
+    while (tc--) {
+        scanf(" %d", &n);
+        for(i = 0; i < n; i++) scanf(" %d", &a[i]);
+        sort(a, a + n);
+        for(i = res = 0; i < n; i++) {
+            for(j = i; j < n && a[j] == a[i]; j++);
+            k = j - i; //counting more same.
+            res += ((k + a[i]) / (a[i] + 1)) * (a[i] + 1);
+            i = j - 1;
+        }
+        printf("Case %d: %d\n", ++cs, res);
+    }
+    return 0;
+}
