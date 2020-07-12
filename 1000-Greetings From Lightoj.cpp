a8ed668f8e8aaaf4f40f12134a77128fa556481c
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2020-07-12 23:56:52 +0600

    Create 1000-Greetings From Lightoj.cpp

---
+++ 1000-Greetings From Lightoj.cpp
@@ -0,0 +1,19 @@
+/*BISMILLAH
+THE WHITE WOLF
+NO DREAM IS TOO BIG AND NO DREAMER IS TOO SMALL*/
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << i << ": " << a + b << endl;
+    }
+    return 0;
+}
